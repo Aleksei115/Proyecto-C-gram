4f56e7e4c6c914de9ea1f1c60fc9eedcc4aa501b
Aleksei115 <alekseigarcia47@gmail.com>
2021-07-25 23:09:18 -0500

    update

--- Programa/funcionalidades/menuDeUsuario/menuDeUsuario.c
+++ Programa/funcionalidades/menuDeUsuario/menuDeUsuario.c
@@ -12,44 +12,64 @@ void menuDeUsuario(usuario usuario_logeado){
         printf("\n****************************************************************************\n");
         printf("\n\t   B I E N V E N I D O   A  TU  MENU    DE  C - G R A M\n");
         printf("\n\t\t       Pantalla Inicial\n\n \nSelecciona una opcion:\n\n");
-        printf("\n1 ---> Agregar Publicaciones \n");
-        printf("\n2 ---> Eliminar Publicaciones \n");
-        printf("\n3 ---> Agregar Amigos \n");
-        printf("\n4 ---> Eliminar Amigos \n");
+        printf("\n1 ---> Ver  tus publicaciones\n");
+        printf("\n2 ---> Agregar Publicaciones  \n");
+        printf("\n3 ---> Eliminar Publicaciones \n");
+        printf("\n4 ---> Agregar Amigos \n");
         printf("\n5 ---> Ver publicaciones de Amigos \n");
-        printf("\n6 ---> Ver  tus publicaciones \n");
-        printf("\n7 ---> Salir de la aplicacion \n");
+        printf("\n6 ---> Eliminar Amigos \n");
+        printf("\n7 ---> Ver el top 5 hashtags usado en México\n");
+        printf("\n8 ---> Salir de la aplicacion \n");
         printf("\n\t\t\t\t\t\t\t Usuario logeado:\n");
         printf("\t\t\t\t\t\t\t %s \n",usuario_logeado.nombre);
 
         printf("\n****************************************************************************\n");
         printf("\n\nIntroduce la opcion: \n");
         
-        break;
-        // if (scanf("%d", &opcion) == 1 ){
+        if (scanf("%d", &opcion) == 1 ){
 
-        //     switch (opcion) {
+            switch (opcion) {
 
-        //         case 1:
-        //             agregarUsuarios();
-        //             break;
+                case 1:
+                    agregarUsuarios();
+                    break;
 
-        //         case 2:
-        //             inicioSesion();
-        //             break;    
+                case 2:
+                    inicioSesion();
+                    break;
 
-        //         case 3:
-        //             printf("Gracias por usar la aplicacion, Adios!!\n");
-        //             exit(1);
+                case 3:
+                    agregarUsuarios();
+                    break;
+
+                case 4:
+                    inicioSesion();
+                    break;  
+
+                case 5:
+                    agregarUsuarios();
+                    break;
+
+                case 6:
+                    inicioSesion();
+                    break;   
+
+                case 7:
+                    inicioSesion();
+                    break;  
+
+                case 8:
+                    printf("Gracias por usar la aplicacion, Adios!!\n");
+                    exit(1);
                     
-        //     }
+            }
 
 
-        // }
-        // else{
-        //     printf("\nOpcion no valida\n Salindo de la aplicacion\n");
-        //     error(2);
-        // }
+        }
+        else{
+            printf("\nOpcion no valida\n Salindo de la aplicacion\n");
+            error(2);
+        }
            
 
     }
